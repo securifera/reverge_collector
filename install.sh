db8933807ed09f93a378acb69356d219efa6b21b
securifera <securifera@gmail.com>
2025-06-24 18:09:48 +0000

    Added poetry

--- install.sh
+++ install.sh
@@ -103,7 +103,7 @@ cp -r ./waluigi /opt/reverge_collector/
 cp ./pyproject.toml /opt/reverge_collector/
 sudo chmod -R 777 /opt/reverge_collector
 cd /opt/reverge_collector 
-python3 -m build
+poetry build
 python3 -m pip install dist/waluigi*.whl
 
 ###############
