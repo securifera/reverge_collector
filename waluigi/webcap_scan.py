5490b738446f7146172f70a803b38ce29e0bd950
Securifera <securifera@gmail.com>
2025-06-24 16:49:28 +0000

    More fixes

--- waluigi/webcap_scan.py
+++ waluigi/webcap_scan.py
@@ -50,55 +50,55 @@ class Webcap(data_model.WaluigiTool):
         return True
 
 
-async def webcap_asyncio(future_map):
+async def webcap_asyncio(future_map, meta_file_path):
 
-    ret_list = []
     # create a browser instance
     browser = Browser(timeout=5)
     # start the browser
     await browser.start()
 
-    for url, scan_tuple in future_map.items():
-        port_id, http_endpoint_data_id, domain_str, path = scan_tuple
-        url_entry = {'port_id': port_id,
-                     'http_endpoint_data_id': http_endpoint_data_id, 'path': path, 'domain': domain_str}
+    with open(meta_file_path, 'w') as f:
+        for url, scan_tuple in future_map.items():
+            port_id, http_endpoint_data_id, domain_str, path = scan_tuple
+            url_entry = {'port_id': port_id,
+                         'http_endpoint_data_id': http_endpoint_data_id, 'path': path, 'domain': domain_str}
 
-        # take a screenshot
-        webscreenshot = None
-        try:
-            webscreenshot = await browser.screenshot(url)
-        except WebCapError as e:
-            if 'You must call start()' in str(e):
-                # Restart the browser
-                browser = Browser(timeout=5)
-                await browser.start()
-                continue
-        except Exception as e:
-            logging.getLogger(__name__).error(
-                f"Error taking screenshot for {url}: {str(e)}")
-            logging.getLogger(__name__).debug(traceback.format_exc())
-            break
+            # take a screenshot
+            webscreenshot = None
+            try:
+                webscreenshot = await browser.screenshot(url)
+            except WebCapError as e:
+                if 'You must call start()' in str(e):
+                    # Restart the browser
+                    browser = Browser(timeout=5)
+                    await browser.start()
+                    continue
+            except Exception as e:
+                logging.getLogger(__name__).error(
+                    f"Error taking screenshot for {url}: {str(e)}")
+                logging.getLogger(__name__).debug(traceback.format_exc())
+                break
 
-        if webscreenshot and webscreenshot.status_code != 0:
-            url_entry['url'] = url
-            url_entry['image_data'] = base64.b64encode(
-                webscreenshot.blob).decode()
-            url_entry['status_code'] = webscreenshot.status_code
-            url_entry['title'] = webscreenshot.title
-            ret_list.append(url_entry)
-        else:
-            logging.getLogger(__name__).warning(
-                f"Failed to take screenshot for {url}")
+            if webscreenshot and webscreenshot.status_code != 0:
+                url_entry['url'] = url
+                url_entry['image_data'] = base64.b64encode(
+                    webscreenshot.blob).decode()
+                url_entry['status_code'] = webscreenshot.status_code
+                url_entry['title'] = webscreenshot.title
+
+                # Write as JSON line
+                f.write(json.dumps(url_entry) + '\n')
+            else:
+                logging.getLogger(__name__).warning(
+                    f"Failed to take screenshot for {url}")
 
     # stop the browser
     await browser.stop()
 
-    return ret_list
 
+def webcap_wrapper(future_map, meta_file_path):
 
-def webcap_wrapper(future_map):
-
-    return asyncio.run(webcap_asyncio(future_map))
+    return asyncio.run(webcap_asyncio(future_map, meta_file_path))
 
 
 def queue_scan(host, port_str, secure, port_id, query_arg="", domain_str=None, http_endpoint_data_id=None):
@@ -261,30 +261,17 @@ class WebcapScan(luigi.Task):
                     logging.getLogger(__name__).debug(
                         "Skipping port %s on host %s as it does not appear to be a web endpoint." % (port_str, ip_addr))
 
-        # Submit the tuples
-        futures = []
-
         # Submit the scan task
+        meta_file = '%s%s%s' % (dir_path, os.path.sep, 'screenshots.json')
         future_inst = scan_utils.executor.submit(
-            webcap_wrapper, future_map)
-        futures.append(future_inst)
+            webcap_wrapper, future_map, meta_file)
 
-        scan_proc_inst = data_model.ToolExecutor(futures)
+        scan_proc_inst = data_model.ToolExecutor([future_inst])
         scheduled_scan_obj.register_tool_executor(
             scheduled_scan_obj.current_tool_instance_id, scan_proc_inst)
 
         # Wait for the tasks to complete and retrieve results
-        ret_list = []
-        for future in futures:
-            ret_list.extend(future.result())
-
-        # Write the results to the output file
-        meta_file = '%s%s%s' % (dir_path, os.path.sep, 'screenshots.json')
-        with open(meta_file, 'w') as f:
-            f.write(json.dumps(ret_list))
-
-        logging.getLogger(__name__).debug(
-            "WebcapScan completed. %d screenshots captured." % len(ret_list))
+        future_inst.result()
 
 
 @inherits(WebcapScan)
@@ -311,139 +298,141 @@ class ImportWebcapOutput(data_model.ImportToolXOutput):
         if os.path.exists(meta_file):
 
             with open(meta_file, 'r') as file_fd:
-                data = file_fd.read()
 
-            count = 0
-            import_data_arr = []
-            screenshot_array = json.loads(data)
-            for screenshot_meta in screenshot_array:
-                ret_arr = []
+                count = 0
+                for line in file_fd:
+                    if not line.strip():
+                        continue
 
-                web_path = screenshot_meta['path']
-                port_id = screenshot_meta['port_id']
-                status_code = screenshot_meta['status_code']
-                screenshot_bytes_b64 = screenshot_meta['image_data']
-                title = screenshot_meta['title']
-                http_endpoint_data_id = screenshot_meta['http_endpoint_data_id']
+                    ret_arr = []
+                    screenshot_meta = json.loads(line)
+                    import_data_arr = []
 
-                # Hash the image
-                screenshot_id = None
-                hash_alg = hashlib.sha1
-                hashobj = hash_alg()
-                hashobj.update(base64.b64decode(screenshot_bytes_b64))
-                image_hash = hashobj.digest()
-                image_hash_str = binascii.hexlify(image_hash).decode()
+                    web_path = screenshot_meta['path']
+                    port_id = screenshot_meta['port_id']
+                    status_code = screenshot_meta['status_code']
+                    screenshot_bytes_b64 = screenshot_meta['image_data']
+                    title = screenshot_meta['title']
+                    http_endpoint_data_id = screenshot_meta['http_endpoint_data_id']
 
-                if image_hash_str in screenshot_hash_map:
-                    screenshot_obj = screenshot_hash_map[image_hash_str]
-                else:
-                    screenshot_obj = data_model.Screenshot()
-                    screenshot_obj.collection_tool_instance_id = tool_instance_id
-                    screenshot_obj.screenshot = screenshot_bytes_b64
-                    screenshot_obj.image_hash = image_hash_str
+                    # Hash the image
+                    screenshot_id = None
+                    hash_alg = hashlib.sha1
+                    hashobj = hash_alg()
+                    hashobj.update(base64.b64decode(screenshot_bytes_b64))
+                    image_hash = hashobj.digest()
+                    image_hash_str = binascii.hexlify(image_hash).decode()
 
-                    # Add to map and the object list
-                    screenshot_hash_map[image_hash_str] = screenshot_obj
-
-                ret_arr.append(screenshot_obj)
-
-                screenshot_id = screenshot_obj.id
-
-                hashobj = hash_alg()
-                hashobj.update(web_path.encode())
-                path_hash = hashobj.digest()
-                hex_str = binascii.hexlify(path_hash).decode()
-                web_path_hash = hex_str
-
-                # Domain key exists and is not None
-                endpoint_domain_id = None
-                if 'domain' in screenshot_meta and screenshot_meta['domain']:
-                    domain_str = screenshot_meta['domain']
-                    if domain_str in domain_name_id_map:
-                        domain_obj = domain_name_id_map[domain_str]
+                    if image_hash_str in screenshot_hash_map:
+                        screenshot_obj = screenshot_hash_map[image_hash_str]
                     else:
-                        domain_obj = data_model.Domain()
-                        domain_obj.collection_tool_instance_id = tool_instance_id
-                        domain_obj.name = domain_str
-                        domain_name_id_map[domain_str] = domain_obj
+                        screenshot_obj = data_model.Screenshot()
+                        screenshot_obj.collection_tool_instance_id = tool_instance_id
+                        screenshot_obj.screenshot = screenshot_bytes_b64
+                        screenshot_obj.image_hash = image_hash_str
 
-                    # Add domain
-                    ret_arr.append(domain_obj)
-                    # Set endpoint id
-                    endpoint_domain_id = domain_obj.id
+                        # Add to map and the object list
+                        screenshot_hash_map[image_hash_str] = screenshot_obj
 
-                if web_path_hash in path_hash_map:
-                    path_obj = path_hash_map[web_path_hash]
-                else:
-                    path_obj = data_model.ListItem()
-                    path_obj.collection_tool_instance_id = tool_instance_id
-                    path_obj.web_path = web_path
-                    path_obj.web_path_hash = web_path_hash
+                    ret_arr.append(screenshot_obj)
 
-                    # Add to map and the object list
-                    path_hash_map[web_path_hash] = path_obj
+                    screenshot_id = screenshot_obj.id
 
-                # Add path object
-                ret_arr.append(path_obj)
+                    hashobj = hash_alg()
+                    hashobj.update(web_path.encode())
+                    path_hash = hashobj.digest()
+                    hex_str = binascii.hexlify(path_hash).decode()
+                    web_path_hash = hex_str
 
-                web_path_id = path_obj.id
+                    # Domain key exists and is not None
+                    endpoint_domain_id = None
+                    if 'domain' in screenshot_meta and screenshot_meta['domain']:
+                        domain_str = screenshot_meta['domain']
+                        if domain_str in domain_name_id_map:
+                            domain_obj = domain_name_id_map[domain_str]
+                        else:
+                            domain_obj = data_model.Domain()
+                            domain_obj.collection_tool_instance_id = tool_instance_id
+                            domain_obj.name = domain_str
+                            domain_name_id_map[domain_str] = domain_obj
 
-                # Add http endpoint
-                http_endpoint_obj = data_model.HttpEndpoint(
-                    parent_id=port_id)
-                http_endpoint_obj.collection_tool_instance_id = tool_instance_id
-                http_endpoint_obj.web_path_id = web_path_id
+                        # Add domain
+                        ret_arr.append(domain_obj)
+                        # Set endpoint id
+                        endpoint_domain_id = domain_obj.id
 
-                # Add the endpoint
-                ret_arr.append(http_endpoint_obj)
+                    if web_path_hash in path_hash_map:
+                        path_obj = path_hash_map[web_path_hash]
+                    else:
+                        path_obj = data_model.ListItem()
+                        path_obj.collection_tool_instance_id = tool_instance_id
+                        path_obj.web_path = web_path
+                        path_obj.web_path_hash = web_path_hash
 
-                # Add http endpoint data
-                http_endpoint_data_obj = data_model.HttpEndpointData(
-                    parent_id=http_endpoint_obj.id)
-                http_endpoint_data_obj.collection_tool_instance_id = tool_instance_id
-                http_endpoint_data_obj.domain_id = endpoint_domain_id
-                http_endpoint_data_obj.status = status_code
-                http_endpoint_data_obj.title = title
-                http_endpoint_data_obj.screenshot_id = screenshot_id
+                        # Add to map and the object list
+                        path_hash_map[web_path_hash] = path_obj
 
-                # Set the object id if the object already exists
-                if http_endpoint_data_id:
-                    http_endpoint_data_obj.id = http_endpoint_data_id
+                    # Add path object
+                    ret_arr.append(path_obj)
 
-                # Add the endpoint
-                ret_arr.append(http_endpoint_data_obj)
+                    web_path_id = path_obj.id
 
-                if len(ret_arr) > 0:
+                    # Add http endpoint
+                    http_endpoint_obj = data_model.HttpEndpoint(
+                        parent_id=port_id)
+                    http_endpoint_obj.collection_tool_instance_id = tool_instance_id
+                    http_endpoint_obj.web_path_id = web_path_id
 
-                    record_map = {}
-                    import_arr = []
-                    for obj in ret_arr:
-                        record_map[obj.id] = obj
-                        flat_obj = obj.to_jsonable()
-                        import_arr.append(flat_obj)
+                    # Add the endpoint
+                    ret_arr.append(http_endpoint_obj)
 
-                    # Import the ports to the manager
-                    updated_record_map = recon_manager.import_data(
-                        scan_id, tool_id, import_arr)
+                    # Add http endpoint data
+                    http_endpoint_data_obj = data_model.HttpEndpointData(
+                        parent_id=http_endpoint_obj.id)
+                    http_endpoint_data_obj.collection_tool_instance_id = tool_instance_id
+                    http_endpoint_data_obj.domain_id = endpoint_domain_id
+                    http_endpoint_data_obj.status = status_code
+                    http_endpoint_data_obj.title = title
+                    http_endpoint_data_obj.screenshot_id = screenshot_id
 
-                    # Update the records
-                    updated_import_arr = data_model.update_scope_array(
-                        record_map, updated_record_map)
+                    # Set the object id if the object already exists
+                    if http_endpoint_data_id:
+                        http_endpoint_data_obj.id = http_endpoint_data_id
 
-                    import_data_arr.extend(updated_import_arr)
+                    # Add the endpoint
+                    ret_arr.append(http_endpoint_data_obj)
 
-                    # Update the scan scope
-                    scheduled_scan_obj.scan_data.update(record_map)
+                    if len(ret_arr) > 0:
 
-                count += 1
+                        record_map = {}
+                        import_arr = []
+                        for obj in ret_arr:
+                            record_map[obj.id] = obj
+                            flat_obj = obj.to_jsonable()
+                            import_arr.append(flat_obj)
 
-            # Write imported data to file
-            tool_import_file = self.output().path
-            with open(tool_import_file, 'w') as import_fd:
-                import_fd.write(json.dumps(import_data_arr))
+                        # Import the ports to the manager
+                        updated_record_map = recon_manager.import_data(
+                            scan_id, tool_id, import_arr)
 
-            logging.getLogger(__name__).debug(
-                "Imported %d screenshots to manager." % (count))
+                        # Update the records
+                        updated_import_arr = data_model.update_scope_array(
+                            record_map, updated_record_map)
+
+                        import_data_arr.extend(updated_import_arr)
+
+                        # Update the scan scope
+                        scheduled_scan_obj.scan_data.update(record_map)
+
+                    count += 1
+
+                    # Write imported data to file
+                    tool_import_file = self.output().path
+                    with open(tool_import_file, 'w') as import_fd:
+                        import_fd.write(json.dumps(import_data_arr))
+
+                logging.getLogger(__name__).debug(
+                    "Imported %d screenshots to manager." % (count))
 
         else:
 
